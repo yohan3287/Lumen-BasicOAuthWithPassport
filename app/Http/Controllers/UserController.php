63149759ce7170620f55898515843c3c61fa6b5b
Yohan <yohan@binus.ac.id>
2021-04-04 19:15:47 +0700

    Add getProfile method in UserController class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -19,11 +20,18 @@ class UserController extends Controller
 
     public function registerUser(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required',
-            'password' => 'required',
-            'confirm_password' => 'required'
-        ]);
+        try {
+            $this->validate($request, [
+                'email' => 'required',
+                'password' => 'required',
+                'confirm_password' => 'required'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                "success!" => false,
+                "data" => 'Input validation error!'
+            ], 400);
+        }
 
         if($request->input('password') == $request->input('confirm_password')) {
             $user = User::create([
@@ -48,7 +56,32 @@ class UserController extends Controller
                 "data" => 'Password not match!'
             ], 400);
         }
+    }
 
+    public function getProfile(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'userID' => 'required'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                "success!" => false,
+                "data" => 'Input validation error!'
+            ], 400);
+        }
 
+        $user = User::where('id', $request->input('userID'))->first();
+        if(!empty($user)) {
+            return response()->json([
+                "success!" => true,
+                "data" => $user
+            ], 200);
+        } else {
+            return response()->json([
+                "success!" => false,
+                "data" => 'Profile not found!'
+            ], 400);
+        }
     }
 }
